4d0d1d1c99562656377a579c8c3fca15eb0fa474
child404 <vladrez1703@gmail.com>
2022-09-14 20:34:30 +0300

    fixed serialize error, started first logic

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-#![allow(unused_imports)]
-#![allow(unused_variables)]
+#![allow(unused_imports, unused_variables)]
 mod dmenu_interface;
 mod monitor_setup;
 use dmenu_interface::DmenuCmd;
-use monitor_setup::{
-    read_config, write_config, Monitor, MonitorDuplicated, MonitorPosition, MonitorSetup,
-    MonitorSetups,
-};
+use monitor_setup::{Monitor, MonitorDuplicated, MonitorPosition, MonitorSetup, MonitorSetups};
 // use winit::event_loop::EventLoop;
 // use xrandr::XHandle;
 
@@ -19,38 +15,53 @@ fn main() {
     // for monitor in monitors {
     //     println!("{:#?}", monitor);
     // }
-    let monitor = Monitor {
-        name: "eDP-1".to_string(),
-        height_px: 1920,
-        width_px: 1200,
-        rate: 120,
-        pos: MonitorPosition {
-            is_related: false,
-            related_pos: "".to_string(),
-            related_name: "".to_string(),
-        },
-        dupl: MonitorDuplicated {
-            is_duplicated: false,
-            name: "".to_string(),
-        },
-        is_primary: true,
-    };
-    let monitor_setup = MonitorSetup {
-        name: "Single monitor".to_string(),
-        monitors: vec![monitor],
-    };
-    let setups = MonitorSetups {
-        setup: vec![monitor_setup],
-    };
-    write_config(setups);
-    // read_config();
+    // let monitor = Monitor {
+    //     name: "eDP-1".to_string(),
+    //     height_px: 1920,
+    //     width_px: 1200,
+    //     rate: 120,
+    //     pos: MonitorPosition {
+    //         is_related: false,
+    //         related_pos: "".to_string(),
+    //         related_name: "".to_string(),
+    //     },
+    //     dupl: MonitorDuplicated {
+    //         is_duplicated: false,
+    //         name: "".to_string(),
+    //     },
+    //     is_primary: true,
+    // };
+    // let monitor_setup = MonitorSetup {
+    //     name: "Single monitor".to_string(),
+    //     monitors: vec![monitor],
+    // };
+    // let setups = MonitorSetups {
+    //     setup: vec![monitor_setup],
+    // };
+    // println!("{:#?}", MonitorSetups::from_config());
+    // println!("{:#?}", setups);
 
-    let start_options = vec![
-        "Auto-detect".to_string(),
-        "Disconnect all".to_string(),
-        "Create new setup".to_string(),
-        "Remove setup".to_string(),
-    ];
-    let res = DmenuCmd::new(start_options, "Choose an option: ".to_string()).exec();
-    println!("{:#?}", &res);
+    // setups.to_toml();
+
+    let user_setups = MonitorSetups::from_config();
+    let start_options = [
+        user_setups.names(),
+        vec![
+            "Auto-detect".to_string(),
+            "Disconnect all".to_string(),
+            "Create new setup".to_string(),
+            "Remove setup".to_string(),
+        ],
+    ]
+    .concat();
+    match DmenuCmd::new(start_options, "Choose an option: ".to_string())
+        .exec()
+        .as_str()
+    {
+        "Auto-detect" => println!("Detecting setup..."),
+        "Disconnect all" => println!("Disconnecting all displays..."),
+        "Create new setup" => println!("Creating new setup..."),
+        "Remove setup" => println!("Removing setup"),
+        _ => {}
+    }
 }
--- src/monitor_setup.rs
+++ src/monitor_setup.rs
@@ -1,24 +1,8 @@
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-#![allow(dead_code)]
+#![allow(unused_imports, unused_variables, dead_code)]
 use serde_derive::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::fmt::Write;
 use std::fs;
-
-pub fn read_config() {
-    let path_to_config = "/home/child404/.config/dm_monitor_setup/monitor_setups.toml";
-    let toml_string =
-        fs::read_to_string(path_to_config).expect("Should have been able to read the file");
-    let config: MonitorSetups =
-        toml::from_str(&toml_string).expect("Correct monitor_setups.toml structure");
-    println!("{:#?}", config);
-}
-
-pub fn write_config(setups: MonitorSetups) {
-    let toml_string = toml::to_string(&setups).expect("Convert MonitorSetups to toml");
-    println!("{:#?}", toml_string)
-}
+use std::io::Write as IoWrite;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MonitorSetups {
@@ -37,9 +21,9 @@ pub struct Monitor {
     pub height_px: u16,
     pub width_px: u16,
     pub rate: u8,
+    pub is_primary: bool,
     pub pos: MonitorPosition,
     pub dupl: MonitorDuplicated,
-    pub is_primary: bool,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -56,9 +40,41 @@ pub struct MonitorDuplicated {
 }
 
 impl MonitorSetups {
+    pub fn from_config() -> Self {
+        let path_to_config = "/home/child404/.config/dm_monitor_setup/monitor_setups.toml";
+        let toml_string =
+            fs::read_to_string(path_to_config).expect("Should have been able to read the file");
+        let setups: MonitorSetups =
+            toml::from_str(&toml_string).expect("Correct monitor_setups.toml structure");
+        setups
+    }
+
+    pub fn get_setup_by(&self, name: String) -> Result<MonitorSetup, ()> {
+        for setup in &self.setup {
+            if setup.name == name {
+                return Ok(setup);
+            }
+        }
+        Err(())
+    }
+
     pub fn to_toml(&self) {
-        let toml_string = toml::to_string(&self).expect("Convert MonitorSetups to toml");
-        println!("{:#?}", toml_string)
+        let path_to_config = "/home/child404/.config/dm_monitor_setup/monitor_setups.toml";
+        let toml_string: String = toml::to_string(&self).expect("Convert MonitorSetups to toml");
+        let mut file = fs::OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(&path_to_config)
+            .unwrap();
+        file.write_all(toml_string.as_bytes())
+            .expect("MonitorSetups written to a file");
+    }
+
+    pub fn names(&self) -> Vec<String> {
+        self.setup
+            .iter()
+            .map(|monitor_setup| monitor_setup.name.clone())
+            .collect::<Vec<String>>()
     }
 }
 
